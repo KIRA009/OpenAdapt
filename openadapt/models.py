3afecefd666bb58825dcf6f28c32c430047134e4
Shohan Dutta Roy <shohan.duttaroy@torchbox.com>
2024-04-20 23:34:29 +0530

    chore: Refactor all uses of config to use the config object from the config module

--- openadapt/models.py
+++ openadapt/models.py
@@ -10,7 +10,8 @@ import mss.base
 import numpy as np
 import sqlalchemy as sa
 
-from openadapt import config, window
+from openadapt import window
+from openadapt.config import config
 from openadapt.db import db
 
 
