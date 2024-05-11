92a4e4daff11b4ba1af2e79e956ed8a53a3cc1fa
Shohan Dutta Roy <shohanduttaroy99@gmail.com>
2024-05-11 22:21:31 +0530

    feat: temp

--- build_scripts/upload_release_artifacts.sh
+++ build_scripts/upload_release_artifacts.sh
@@ -1,5 +1,7 @@
+poetry install
 VERSION="v$(poetry run python -m build_scripts.get_version)"
-mv dist/OpenAdapt.zip dist/OpenAdapt-$VERSION.zip
-mv dist/OpenAdapt.app.zip dist/OpenAdapt-$VERSION.app.zip
-echo "Uploading release artifacts for version $VERSION"
-gh release upload $VERSION dist/OpenAdapt-$VERSION.zip dist/OpenAdapt-$VERSION.app.zip
+echo "Building release artifacts for version $VERSION"
+# mv dist/OpenAdapt.zip dist/OpenAdapt-$VERSION.zip
+# mv dist/OpenAdapt.app.zip dist/OpenAdapt-$VERSION.app.zip
+# echo "Uploading release artifacts for version $VERSION"
+# gh release upload $VERSION dist/OpenAdapt-$VERSION.zip dist/OpenAdapt-$VERSION.app.zip
